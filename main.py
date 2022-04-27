0efe2a76488332d74e2ead8865b219f9a9f44e49
marvintt <marvintt@hotmail.com>
2022-04-27 15:44:37 -0500

    Screen
    Se crea archivo main.py que contiene el escenario

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+import time
+
+# Crear el escenario
+screen = Screen()  # Instanciar objeto
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Proyecto Juego Snake")
+
+#Final
+screen.exitonclick()
