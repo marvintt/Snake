fdb513c358c7a64048a030384c98874644f29532
marvintt <marvintt@hotmail.com>
2022-04-28 13:11:13 -0500

    Snake collision

--- main.py
+++ main.py
@@ -46,6 +46,14 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+    
+    # Detectar colision con cola
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 # Final
 screen.exitonclick()
